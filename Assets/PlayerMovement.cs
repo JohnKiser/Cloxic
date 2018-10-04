b7db2b97bd7845c6b9faea78065b4d1a8917623a
John Kiser <john.j.kiser@gmail.com>
2018-10-04 09:30:18 -0600

    Made game

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerMovement : NetworkBehaviour
+{
+    void Update()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        float x = .1f;
+
+        if (Input.GetKey("d"))
+        {
+            transform.Translate(x, 0, 0);
+            
+        }
+
+        else if (Input.GetKey("a"))
+        {
+            transform.Translate(-x, 0, 0);
+            
+        }
+
+        else if (Input.GetKey("w"))
+        {
+            transform.Translate(0, 0, x);
+            
+        }
+
+        else if (Input.GetKey("s"))
+        {
+            transform.Translate(0, 0, -x);
+            
+        }
+        //var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
+        //var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        //transform.Translate(x, 0, z);        
+
+    }
+}
